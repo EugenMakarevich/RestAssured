da0b469ceb4aaa3fef0d5a52963a1cd18e7338fc
EugenMakarevich <eugenmakarevich@coherentsolutions.com>
2025-10-31 11:58:08 +0100

    19. Assertions on Json Response body and Headers through Automation code

--- RestAssured/src/test/java/AddPlace.java
+++ RestAssured/src/test/java/AddPlace.java
@@ -1,4 +1,7 @@
+import files.Payload;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class AddPlace {
     public static void main(String[] args) {
@@ -9,28 +12,15 @@ public class AddPlace {
                 .baseUri("https://rahulshettyacademy.com")
                 .queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Eugen house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}")
+                .body(Payload.addPlace())
                 .when()
                 .post("/maps/api/place/add/json")
                 .then()
                 .log()
                 .all()
                 .assertThat()
-                .statusCode(200);
+                .statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.52 (Ubuntu)");
     }
 }
