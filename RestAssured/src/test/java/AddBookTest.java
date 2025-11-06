4c1a4b6db32318b5f2e4e035cd9947617a47ad9b
EugenMakarevich <eugenmakarevich@coherentsolutions.com>
2025-11-06 20:48:04 +0100

    section 7

--- RestAssured/src/test/java/AddBookTest.java
+++ RestAssured/src/test/java/AddBookTest.java
@@ -1,12 +1,22 @@
 import files.Payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class AddBook {
-    @Test
+public class AddBookTest {
+    @DataProvider(name = "bookData")
+    public Object[][] addBookData() {
+        return new Object[][]{
+                {"abc", "7777"},
+                {"def", "6666"},
+                {"gkl", "5555"}
+        };
+    }
+
+    @Test(dataProvider = "bookData")
     public void addBookTest() {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         JsonPath addBookResponse = given()
