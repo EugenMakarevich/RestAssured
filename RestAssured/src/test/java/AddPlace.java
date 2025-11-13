169bc9a0cf1710274743b909ced4b089a07063f5
EugenMakarevich <eugenmakarevich@coherentsolutions.com>
2025-11-13 21:12:08 +0100

    section 7

--- RestAssured/src/test/java/AddPlace.java
+++ RestAssured/src/test/java/AddPlace.java
@@ -2,6 +2,10 @@ import files.Payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.testng.AssertJUnit.assertEquals;
@@ -36,6 +40,33 @@ public class AddPlace {
 
         JsonPath json = new JsonPath(response); // for parsing json
         String placeId = json.getString("place_id");
+
+        // Add place (JSON file)
+        String jsonBody;
+        try {
+            jsonBody = new String(Files.readAllBytes(Paths.get("C:\\Users\\EugenMakarevich\\IdeaProjects\\RestAssuredUdemi\\RestAssured\\src\\main\\resources\\addPlace.json")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        given()
+                .log()
+                .all()
+                .queryParam("key", key)
+                .header("Content-Type", "application/json")
+                .body(jsonBody)
+                .when()
+                .post("/maps/api/place/add/json")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.52 (Ubuntu)")
+                .extract()
+                .response()
+                .asString();
 
         // Update Place
         given()
